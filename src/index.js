d4a74ddedfb2fd7572af74b34421ffe9d35e0cd1
sein <44532313+seinnlee@users.noreply.github.com>
2021-07-29 11:52:44 -0700

    Learned react component using Hello

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello.js';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello greeting = {'Hello' + ' React Ninja'} />
   </React.StrictMode>,
   document.getElementById('root')
 );
